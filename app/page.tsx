88779451b60a440ead2ac474dd9ad32309d33e9c
Diego Sarmiento <dlsarmiento@gmail.com>
2025-07-20 11:53:37 -0400

    Updated slug

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ChatbotWidget from "@/components/ChatbotWidget"
 
 export default function Home() {
     
-  const slug = 'rocketch' // Your store slug, check rocketch.com
+  const slug = 'rocketch-store' // Your store slug, check rocketch.com
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
